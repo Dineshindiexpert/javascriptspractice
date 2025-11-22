7752a8accd6417e46b483c36a1818d6f95ad4fe3
dineshindixpert <dineshjangra0062@gmail.com>
2025-11-22 10:57:59 +0530

    try somethings new

--- first/condition.js
+++ first/condition.js
@@ -1,2 +1,12 @@
-let username = prompt("enter the username below");
-console.log('@'+username+ username.length);
+function changeColor() {
+
+    // Change ID #heading
+    document.querySelectorAll("#heading").forEach(el => {
+        el.style.color = "pink";
+    });
+
+    // Change elements with class .heading-class
+    document.querySelectorAll(".heading-class").forEach(el => {
+        el.style.color = "blue";
+    });
+}
